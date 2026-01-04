e6798f2f0322f21b0bf131a2ad9a0d870fddb309
MaxProgramist <ls555431k@gmail.com>
2026-01-04 20:48:52 +0200

    Final things...

--- Assets/Scripts/Level objects/ButtonManager.cs
+++ Assets/Scripts/Level objects/ButtonManager.cs
@@ -3,6 +3,13 @@ using UnityEngine;
 
 public class ButtonManager : MonoBehaviour
 {
+    [SerializeField] private GameObject tutorial;
+
+    private void Start()
+    {
+        if (tutorial != null) tutorial.SetActive(false);
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision != null)
@@ -21,6 +28,8 @@ public class ButtonManager : MonoBehaviour
             if (collision.CompareTag("Box"))
             {
                 GetComponent<ActivationManager>().Interaction();
+
+                if (tutorial != null) tutorial.SetActive(true);
             }
         }
     }
